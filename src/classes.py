45349743a49ce0cb5dadedc08cfa54e953dce901
cet <cets021@gmail.com>
2023-02-26 00:53:39 +0900

    wip

--- src/classes.py
+++ src/classes.py
@@ -2,7 +2,7 @@ from typing import Any, NoReturn
 
 
 class Employee:
-    def __init__(self, joined: int) -> NoReturn:
+    def __init__(self, joined: int):
         self.joined = joined
 
     roles = [
@@ -21,24 +21,33 @@ class Employee:
         self,
         role: Any,
         hourly_salary: int,
-        calc_type: int,
+        sum_type: int,
         is_include_allowance: bool = True
     ):
         '''
         Args:
             @role: 役職
             @hourly_salary: 時給
-            @calc_type: 0: 月給, 1: 年給
+            @sum_type: 0: 月給, 1: 年給
             @is_include_allowance: 役職手当の有無
         '''
         def calc(_role: int):
             if role == self.roles[_role] | _role:
-                if is_include_allowance:
-                    if calc_type == 0:
-                        return hourly_salary * 30 + hourly_salary * _role
-                    elif calc_type == 1:
-                        return hourly_salary * 365 + hourly_salary * _role
-        # manager, leader, staff
+                def auto_calc(days: int, is_include: bool):
+                    if is_include:
+                        return hourly_salary * days + hourly_salary * _role
+                    else:
+                        return hourly_salary * days
+                if is_include_allowance:  # 役職手当込み
+                    if sum_type == 0:  # 月収
+                        return auto_calc(30, True)
+                    elif sum_type == 1:  # 年収
+                        return auto_calc(365, True)
+                else:  # ウィズアウト役職手当
+                    if sum_type == 0:
+                        return auto_calc(30, False)
+                    elif sum_type == 1:
+                        return auto_calc(365, False)
         calc_salaries = []
         for i in range(3):
             calc_salaries.append(calc(i))
